4634d33498e9152a8ed04976c3aaedbe8610c0db
jon-zawada <jonathan_zawada@yahoo.com>
2021-08-26 16:08:16 -0700

    adds classes and container to Navs html prior to css

--- client/src/components/Navigation/index.jsx
+++ client/src/components/Navigation/index.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 
 const Nav = () => (
-  <nav>
-    <ul>
-      <li><a href="/">Home</a></li>
-      <li><a href="about">About</a></li>
-      <li><a href="book">Book</a></li>
-      <li><a href="faq">FAQ</a></li>
-    </ul>
-  </nav>
+  <div className="nav-container">
+    <nav className="nav-bar">
+      <h3 className="nav-title">Navigation</h3>
+      <ul className="nav-list">
+        <li><a href="/">Home</a></li>
+        <li><a href="about">About</a></li>
+        <li><a href="book">Book</a></li>
+        <li><a href="faq">FAQ</a></li>
+      </ul>
+    </nav>
+  </div>
 );
 
 export default Nav;
