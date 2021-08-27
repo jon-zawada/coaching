2f05e090715a470b375909ecffd98528cd0bb3db
jon-zawada <jonathan_zawada@yahoo.com>
2021-08-27 14:21:16 -0700

    clean up navigation with pages Home and Booking

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,10 +9,6 @@ function App() {
   return (
     <Router>
       <Nav />
-      <div className="background">
-        <h2>Welcome to Jangos Coaching!</h2>
-        <h3>Date: { new Date().toDateString() }</h3>
-      </div>
       <Switch>
         <Route path="/" exact component={Home} />
         <Route path="/booking" component={Booking} />
