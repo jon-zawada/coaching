6df6a5d83828aa461bb9776309f2cb409b3dfd18
jon-zawada <jonathan_zawada@yahoo.com>
2021-08-27 16:35:56 -0700

    month list component deprecated

--- client/src/components/Calendar/MonthList.jsx
+++ client/src/components/Calendar/MonthList.jsx
@@ -1,18 +1,18 @@
-import React from 'react';
+// import React from 'react';
 
-const MonthList = ({ months, setMonth }) => {
-  const popup = months.map((month) => (
-    <div key={month}>
-      <a href="#" onClick={(e) => {setMonth(month)}}>
-        {month}
-      </a>
-    </div>
-  ));
-  return (
-    <div className="month-popup">
-      {popup}
-    </div>
-  );
-};
+// const MonthList = ({ months, setMonth }) => {
+//   const popup = months.map((month) => (
+//     <div key={month}>
+//       <a href="#" onClick={(e) => {setMonth(month)}}>
+//         {month}
+//       </a>
+//     </div>
+//   ));
+//   return (
+//     <div className="month-popup">
+//       {popup}
+//     </div>
+//   );
+// };
 
-export default MonthList;
+// export default MonthList;
