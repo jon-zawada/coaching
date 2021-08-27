8dfbb381f3f71987a759ad78abce71c2d1f75acd
jon-zawada <jonathan_zawada@yahoo.com>
2021-08-27 12:09:43 -0700

    fixes nav links to react router Links

--- client/src/components/Navigation/index.jsx
+++ client/src/components/Navigation/index.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 const Nav = () => (
   <div className="nav-container">
     <nav className="nav-bar">
       <h3 className="nav-title">Navigation</h3>
       <ul className="nav-list">
-        <li><a href="/">Home</a></li>
-        <li><a href="about">About</a></li>
-        <li><a href="book">Book</a></li>
-        <li><a href="faq">FAQ</a></li>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="about">About</Link></li>
+        <li><Link to="book">Book</Link></li>
+        <li><Link to="faq">FAQ</Link></li>
       </ul>
     </nav>
   </div>
