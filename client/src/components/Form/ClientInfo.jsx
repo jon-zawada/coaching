33dc7284e8593014b4cc1c180c43fbbb5b2e6d0b
jon-zawada <jonathan_zawada@yahoo.com>
2021-09-11 03:19:29 -0700

    adds start of form2 css

--- client/src/components/Form/ClientInfo.jsx
+++ client/src/components/Form/ClientInfo.jsx
@@ -1,20 +1,34 @@
 import React from 'react';
 
 const ClientInfo = ({ changeHandler, name, ign, discord, region, email, next, prev }) => (
-  <div className="form2-container">
-    <form>
-      <label>Name</label>
-      <input type="text" name="name" onChange={changeHandler} defaultValue={name} />
-      <label>Email</label>
-      <input type="text" name="email" onChange={changeHandler} defaultValue={email} />
-      <label>League In Game Name</label>
-      <input type="text" name="ign" onChange={changeHandler} defaultValue={ign} />
-      <label>Discord</label>
-      <input type="text" name="discord" onChange={changeHandler} defaultValue={discord} />
-      <label>Region</label>
-      <input type="text" name="region" onChange={changeHandler} defaultValue={region} />
-      <button type="button" className="btn" onClick={prev}>Prev</button>
-      <button type="button" className="btn" onClick={next}>Next</button>
+  <div>
+    <form className="form2">
+      <div className="form2-labels-list">
+        <div className="label-input">
+          <label>Name:</label>
+          <input type="text" name="name" onChange={changeHandler} defaultValue={name} />
+        </div>
+        <div className="label-input">
+          <label>Email:</label>
+          <input type="text" name="email" onChange={changeHandler} defaultValue={email} />
+        </div>
+        <div className="label-input">
+          <label>Summoner Name:</label>
+          <input type="text" name="ign" onChange={changeHandler} defaultValue={ign} />
+        </div>
+        <div className="label-input">
+          <label>Discord:</label>
+          <input type="text" name="discord" onChange={changeHandler} defaultValue={discord} />
+        </div>
+        <div className="label-input">
+          <label>Region:</label>
+          <input type="text" name="region" onChange={changeHandler} defaultValue={region} />
+        </div>
+      </div>
+      <div className="form2-button-container">
+        <button type="button" className="btn" onClick={prev}>Prev</button>
+        <button type="button" className="btn" onClick={next}>Next</button>
+      </div>
     </form>
   </div>
 );
