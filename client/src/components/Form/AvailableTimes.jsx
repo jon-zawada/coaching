2412ecef634de6895cc0c4dba3bc1a6b12e6e561
jon-zawada <jonathan_zawada@yahoo.com>
2021-09-10 18:04:42 -0700

    adds first complete implemetation of form1 css

--- client/src/components/Form/AvailableTimes.jsx
+++ client/src/components/Form/AvailableTimes.jsx
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-const AvailableTimes = ({ times, time, next, updateTime }) => (
-  <div className="form1-container">
-    <ul className="available-times">
-      {times.map((time, i) => (
-        <li key={i} onClick={updateTime}>{time}</li>
-      ))}
-    </ul>
-    <button className="btn" onClick={next} disabled={time === ''}>Next</button>
-  </div>
-);
+const AvailableTimes = ({ times, time, next, updateTime }) => {
+  const [activeIndex, setIndex] = useState(null);
+  return (
+    <div className="form1-container">
+      <ul className="available-times">
+        {times.map((t, i) => {
+          const className = i === activeIndex ? 'active' : null;
+          return (
+            <li
+              key={i}
+              onClick={(e) => {
+                updateTime(e);
+                setIndex(i);
+              }}
+              className={className}
+            >
+              {t}
+            </li>
+          );
+        })}
+      </ul>
+      <button className="btn" onClick={next} disabled={time === ''}>Next</button>
+    </div>
+  );
+}
 
 export default AvailableTimes;
